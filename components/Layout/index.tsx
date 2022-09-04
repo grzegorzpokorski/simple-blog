827b3528e3bcf0c05f3deb3924b3ca1e5acc8df8
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-09-04 16:17:58 +0200

    add Layout component

---
+++ components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import { Footer } from "../Footer";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      {children}
+      <Footer />
+    </>
+  );
+};
