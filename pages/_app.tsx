2beb914a957ca5e1b6e944a98092487b7ea3525b
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-09-05 13:24:33 +0200

    configure and add appolo/client to application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/main.scss";
 import type { AppProps } from "next/app";
+import { client } from "../utils/apolloClient";
+import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
